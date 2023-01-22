12de853e45547cb237d2cd08aafbc4a82fdc19de
Shanila Nehlin <nehlinshanila@gmail.com>
2023-01-22 22:22:34 +0600

    push

--- practice/airline/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_views, name= "login"),
-    path("logout", views.logout_views, name="logout")
-]
